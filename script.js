e407322d47352f6c73de2db8e47635f0e19dbb4f
Edwin Avalos <eoavalos@asu.edu>
2015-03-28 20:05:59 -0700

    First commit. Has some of the basic functionality for the website

---
+++ script.js
@@ -0,0 +1,22 @@
+function showDef()
+{	
+	document.getElementById("tb").innerHTML = document.getElementById("tb1").innerHTML;
+}
+
+function swap(id1, id2)
+{
+	temp = document.getElementById(id1).innerHTML;
+	document.getElementById(id2).innerHTML = temp;
+}
+
+function color(id, bool)
+{
+	if(bool)
+	{
+		id.style.color="red";
+	}
+	else
+	{
+		id.style.color="black";
+	}
+}
